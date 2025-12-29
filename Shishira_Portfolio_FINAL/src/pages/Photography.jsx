ab22dc23532c9301dccad50879b0210c3c393c28
Shishira B J <93389074+Inevitableshishira@users.noreply.github.com>
2025-12-29 14:32:16 +0530

    Delete Shishira_Portfolio_FINAL directory

--- Shishira_Portfolio_FINAL/src/pages/Photography.jsx
+++
@@ -1,7 +0,0 @@
-export default function Photography() {
-  return (
-    <section className="min-h-screen flex items-center justify-center text-gray-400">
-      Photography section coming soon.
-    </section>
-  );
-}
